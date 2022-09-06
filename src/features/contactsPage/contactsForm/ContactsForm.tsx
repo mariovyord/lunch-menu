a391ccb9bd98282816243b43763afd3813862cf2
NecroBread <mariovyordanov@gmail.com>
2022-09-06 16:37:41 +0300

    add: google maps and create contacts data component

--- src/features/contactsPage/contactsForm/ContactsForm.tsx
+++ src/features/contactsPage/contactsForm/ContactsForm.tsx
@@ -17,7 +17,7 @@ const ContactsForm = () => {
 			</div>
 			<div className='mt-2 px-2'>
 				<label htmlFor="message">Message</label>
-				<textarea className='border-2 rounded-lg w-full mt-2 p-2 text-black' name="message" id="message" placeholder='ex.: Hello, I love your ravioli!'></textarea>
+				<textarea className='border-2 rounded-lg w-full min-h-[125px] mt-2 p-2 text-black' name="message" id="message" placeholder='ex.: Hello, I love your ravioli!'></textarea>
 			</div>
 			<div className='flex justify-end mt-2 px-2'>
 				<input className='cursor-pointer flex h-12 w-full items-center justify-center rounded-lg bg-accent px-6 font-semibold text-accent-content transition-all hover:bg-accent-focus hover:shadow active:scale-95 sm:w-auto' type="submit" value="Send" />
