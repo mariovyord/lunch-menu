323cd003f5f1cc52242215f4e588c27fad8d36de
NecroBread <mariovyordanov@gmail.com>
2022-09-06 15:48:30 +0300

    create: contact form layout

---
+++ src/features/contactsPage/contactsForm/ContactsForm.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ContactsForm = () => {
+	return (
+		<form className='text-base'>
+			<div className='w-full sm:w-1/2 inline-block px-2'>
+				<label htmlFor="name">Name</label>
+				<input className='border-2 rounded-lg w-full mt-2 p-2' type="text" id="name" name="name" placeholder="ex.: John Johnson" />
+			</div>
+			<div className='w-full sm:w-1/2 inline-block px-2'>
+				<label htmlFor="email">Email</label>
+				<input className='border-2 rounded-lg w-full mt-2 p-2' type="email" id="email" name="email" placeholder='ex.: john@email.com' />
+			</div>
+			<div className='mt-2 px-2'>
+				<label htmlFor="message">Message</label>
+				<textarea className='border-2 rounded-lg w-full mt-2 p-2' name="message" id="message" placeholder='ex.: Hello, I love your ravioli!'></textarea>
+			</div>
+			<div className='flex justify-end mt-2 px-2'>
+				<input className='cursor-pointer flex h-12 w-full items-center justify-center rounded-lg bg-accent px-6 font-semibold text-accent-content transition-all hover:bg-accent-focus hover:shadow active:scale-95 sm:w-auto' type="submit" value="Send" />
+			</div>
+		</form>
+	)
+}
+
+export default ContactsForm
