021f827ae8282df7614f4e73136a02c6bf967983
NecroBread <mariovyordanov@gmail.com>
2022-09-06 15:06:59 +0300

    create: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import ContactsPage from "./features/contactsPage/ContactsPage";
 import Footer from "./features/footer/Footer";
+import HomePage from "./features/homePage/HomePage";
 import NavigationBar from "./features/navigationBar/NavigationBar";
 
 function App() {
 	return (
 		<div className="min-h-screen flex flex-col">
 			<NavigationBar />
-			<main className="max-w-5xl mx-auto">Hello World</main>
+			<main className="max-w-5xl mx-auto px-3">
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/contacts' element={<ContactsPage />} />
+				</Routes>
+			</main>
 			<Footer />
 		</div>
 	);
