e951e8da5bfd321e5a8915078cfc99a62863199d
NecroBread <mariovyordanov@gmail.com>
2022-09-07 21:30:42 +0300

    add: basic scroll into view on load

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,8 +1,34 @@
 import SideMenu from './sideMenu/SideMenu';
 import ScrollSpy from "react-ui-scrollspy";
 import DayMenuCard from './dayMenuCard/DayMenuCard';
+import { useEffect, useRef } from 'react';
+import { useAppSelector } from '../../app/hooks';
+import { selectDay } from '../../app/slices/daySlice';
 
 const HomePage = () => {
+	const day = useAppSelector(selectDay);
+
+	const monday = useRef<HTMLDivElement>(null);
+	const tuesday = useRef<HTMLDivElement>(null);
+	const wednesday = useRef<HTMLDivElement>(null);
+	const thursday = useRef<HTMLDivElement>(null);
+	const friday = useRef<HTMLDivElement>(null);
+	const saturday = useRef<HTMLDivElement>(null);
+	const sunday = useRef<HTMLDivElement>(null);
+
+	const refs = [
+		monday,
+		tuesday,
+		wednesday,
+		thursday,
+		friday,
+		saturday,
+		sunday
+	];
+
+	useEffect(() => {
+		refs[day.code].current?.scrollIntoView();
+	})
 	return (
 		<div className='grid grid-cols-6'>
 			<div className='sticky col-span-6 sm:col-span-1 top-0 left-0 sm:h-screen text-2xl bg-slate-200'>
@@ -12,19 +38,19 @@ const HomePage = () => {
 				<ScrollSpy
 					useBoxMethod={false}
 				>
-					<DayMenuCard dayCode={0} />
-					<DayMenuCard dayCode={1} />
-					<DayMenuCard dayCode={2} />
-					<DayMenuCard dayCode={3} />
-					<DayMenuCard dayCode={4} />
+					<DayMenuCard reference={refs[0]} dayCode={0} />
+					<DayMenuCard reference={refs[1]} dayCode={1} />
+					<DayMenuCard reference={refs[2]} dayCode={2} />
+					<DayMenuCard reference={refs[3]} dayCode={3} />
+					<DayMenuCard reference={refs[4]} dayCode={4} />
 
-					<div className='pt-20 sm:pt-10' id="saturday">
+					<div ref={refs[5]} className='pt-20 sm:pt-10' id="saturday">
 						<h2 className='font-bold text-3xl text-primary border-b-2 pb-2' ><a href="#saturday">Saturday</a></h2>
 						<div className='uppercase p-4 text-2xl italic flex gap-4'>
 							<p>Rest Day</p>
 						</div>
 					</div>
-					<div className='py-10' id="sunday">
+					<div ref={refs[6]} className='py-10' id="sunday">
 						<h2 className='font-bold text-3xl text-primary border-b-2 pb-2'><a href="#sunday">Sunday</a></h2>
 						<div className='uppercase p-4 text-2xl italic flex gap-4'>
 							<p>Rest Day</p>
--- src/features/homePage/dayMenuCard/DayMenuCard.tsx
+++ src/features/homePage/dayMenuCard/DayMenuCard.tsx
@@ -4,11 +4,11 @@ import { getDayStringFromCode } from '../../../utils/getDayStringFromCode';
 import { useAppSelector } from '../../../app/hooks';
 import { selectDay } from '../../../app/slices/daySlice';
 
-const DayMenuCard = ({ dayCode }: { dayCode: number }) => {
+const DayMenuCard = ({ dayCode, reference }: { dayCode: number, reference: any }) => {
 	const currentDay = useAppSelector(selectDay);
 
 	return (
-		<div className='pt-20 sm:pt-10' id={getDayStringFromCode(dayCode).toLowerCase()}>
+		<div ref={reference} className='pt-20 sm:pt-10' id={getDayStringFromCode(dayCode).toLowerCase()}>
 			<h2 className='font-bold text-3xl text-primary border-b-2 pb-2' ><a href={`#${getDayStringFromCode(dayCode).toLowerCase()}`}>{getDayStringFromCode(dayCode)}</a></h2>
 			<div className='px-4 py-2 flex flex-col gap-1'>
 				{MENU[dayCode].map(pr => <ProductCard key={pr.title} active={dayCode === currentDay.code} product={pr} />)}
