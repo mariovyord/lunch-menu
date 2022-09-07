644355bb94c27dc23d167421b805d3c6233bf783
NecroBread <mariovyordanov@gmail.com>
2022-09-07 16:56:36 +0300

    add: scroll spy lib

--- src/features/homePage/HomePage.tsx
+++ src/features/homePage/HomePage.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import ProductCard from './productCard/ProductCard';
-import MENU from '../../assets/data/menu.json';
 import SideMenu from './sideMenu/SideMenu';
+import ScrollSpy from "react-ui-scrollspy";
+import DayMenuCard from './dayMenuCard/DayMenuCard';
 
 const HomePage = () => {
 	return (
@@ -10,48 +9,28 @@ const HomePage = () => {
 				<SideMenu />
 			</div>
 			<div className='col-span-6 sm:col-span-5 px-4 sm:px-8 bg-white'>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 py-4' id="monday"><a href="#monday">Monday</a></h2>
-					<div className='px-4 py-2 flex flex-col gap-1'>
-						{MENU[0].map(pr => <ProductCard product={pr} />)}
+				<ScrollSpy
+					useBoxMethod={false}
+				>
+					<DayMenuCard menuCode={0} day="Monday" />
+					<DayMenuCard menuCode={1} day="Tuesday" />
+					<DayMenuCard menuCode={2} day="Wednesday" />
+					<DayMenuCard menuCode={3} day="Thursday" />
+					<DayMenuCard menuCode={4} day="Friday" />
+
+					<div className='pt-20 sm:pt-10' id="saturday">
+						<h2 className='font-bold text-3xl text-primary border-b-2 pb-2' ><a href="#saturday">Saturday</a></h2>
+						<div className='uppercase p-4 text-2xl italic flex gap-4'>
+							<p>Rest Day</p>
+						</div>
 					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="tuesday"><a href="#tuesday">Tuesday</a></h2>
-					<div className='px-4 py-2 flex flex-col gap-1'>
-						{MENU[1].map(pr => <ProductCard product={pr} />)}
+					<div className='py-10' id="sunday">
+						<h2 className='font-bold text-3xl text-primary border-b-2 pb-2'><a href="#sunday">Sunday</a></h2>
+						<div className='uppercase p-4 text-2xl italic flex gap-4'>
+							<p>Rest Day</p>
+						</div>
 					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="wednesday"><a href="#wednesday">Wednesday</a></h2>
-					<div className='px-4 py-2 flex flex-col gap-1'>
-						{MENU[2].map(pr => <ProductCard product={pr} />)}
-					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="thursday"><a href="#thursday">Thursday</a></h2>
-					<div className='px-4 py-2 flex flex-col gap-1'>
-						{MENU[3].map(pr => <ProductCard product={pr} />)}
-					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="friday"><a href="#friday">Friday</a></h2>
-					<div className='px-4 py-2 flex flex-col gap-1'>
-						{MENU[4].map(pr => <ProductCard product={pr} />)}
-					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="saturday"><a href="#saturday">Saturday</a></h2>
-					<div className='uppercase p-4 text-2xl italic flex gap-4'>
-						<p>Rest Day</p>
-					</div>
-				</div>
-				<div className='mb-6'>
-					<h2 className='font-bold text-3xl text-primary border-b-2 pb-4' id="sunday"><a href="#sunday">Sunday</a></h2>
-					<div className='uppercase p-4 text-2xl italic flex gap-4'>
-						<p>Rest Day</p>
-					</div>
-				</div>
+				</ScrollSpy>
 			</div>
 		</div>
 	)
--- src/features/homePage/sideMenu/SideMenu.tsx
+++ src/features/homePage/sideMenu/SideMenu.tsx
@@ -1,13 +1,11 @@
 const SideMenu = () => {
 	return (
-		<ul className='overflow-scroll py-2 sm:overflow-hidden'>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#monday">Monday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a href="#tuesday">Tuesday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#wednesday">Wednesday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#thursday">Thursday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#friday">Friday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#saturday">Saturday</a></li>
-			<li className='inline sm:block border-b-2 w-full p-4'><a className='' href="#sunday">Sunday</a></li>
+		<ul className='overflow-x-scroll overflow-y-hidden sm:overflow-hidden sm:py-0 py-4 top-0'>
+			<li data-to-scrollspy-id="monday" className='text-slate-500 inline sm:block border-b-2 w-full p-4'><a href="#monday">Monday</a></li>
+			<li data-to-scrollspy-id="tuesday" className='text-slate-500 inline sm:block border-b-2 w-full p-4'><a href="#tuesday">Tuesday</a></li>
+			<li data-to-scrollspy-id="wednesday" className='text-slate-500 inline sm:block border-b-2 w-full p-4'><a href="#wednesday">Wednesday</a></li>
+			<li data-to-scrollspy-id="thursday" className='text-slate-500 inline sm:block border-b-2 w-full p-4'><a href="#thursday">Thursday</a></li>
+			<li data-to-scrollspy-id="friday" className='text-slate-500 inline sm:block border-b-2 w-full p-4'><a href="#friday">Friday</a></li>
 		</ul>
 	)
 }
