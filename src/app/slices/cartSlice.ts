1bb4e344edbeb8bc318296473737f4692f70f3b5
NecroBread <mariovyordanov@gmail.com>
2022-09-08 13:38:03 +0300

    add: add and change reducers

--- src/app/slices/cartSlice.ts
+++ src/app/slices/cartSlice.ts
@@ -1,10 +1,9 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { TCartProduct } from '../../types/types';
 import { RootState } from '../store';
 
-type TItem = any;
-
 type TCartState = {
-	items: TItem[],
+	items: TCartProduct[],
 	toggle: boolean,
 }
 
@@ -18,10 +17,37 @@ export const cartSlice = createSlice({
 	initialState: getInitialState,
 	reducers: {
 		addItem: (state, action: PayloadAction<any>) => {
-			state.items.push(action.payload);
+			const newItem = {
+				title: action.payload.title,
+				qty: 1,
+				price: action.payload.price
+			}
+
+			const existing = state.items.find(x => x.title === newItem.title);
+			if (existing) {
+				existing.qty++;
+			} else {
+				state.items.push(newItem);
+			}
 		},
 		removeItem: (state, action: PayloadAction<any>) => {
 
+		},
+		changeQty: (state, action: PayloadAction<TCartProduct>) => {
+			const existing = state.items.find(x => x.title === action.payload.title);
+			if (existing) {
+				if (action.payload.qty <= 0) {
+					const index = state.items.indexOf(existing);
+					const firstPart = state.items.slice(0, index);
+					const secondPart = state.items.slice(index + 1);
+					state.items = [...firstPart, ...secondPart];
+				} else {
+					existing.qty = action.payload.qty;
+				}
+			} else {
+				console.log('no such item');
+				// TODO add error notification
+			}
 		},
 		toggle: (state) => {
 			state.toggle = !state.toggle;
