b3aff6ce6e05a1f4f97167a09778a00b5d3306f8
NecroBread <mariovyordanov@gmail.com>
2022-09-08 09:45:18 +0300

    adjustments

--- src/features/navigationBar/NavigationBar.tsx
+++ src/features/navigationBar/NavigationBar.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import Cart from '../cart/Cart';
 
 type listItemArray = {
 	name: string,
@@ -30,36 +31,40 @@ const NavigationBar = () => {
 	]
 
 	return (
-		<header className='sticky top-0 h-16 bg-lime-700 shadow'>
+		<header className='sticky top-0 h-16 bg-lime-700 shadow z-50'>
 			<div className='max-w-7xl m-auto h-full'>
 				<div className='flex justify-between min-w-full h-full px-3'>
 					<div className='text-white font-bold text-3xl h-full'>
 						<NavLink to='/' className='h-full flex items-center'>TASTY</NavLink>
 					</div>
-					<nav>
-						<div className='hidden sm:block w-full h-full'>
-							<ul className='flex h-full'>
-								{list.map((item) => {
-									return <li key={item.url}><NavLink to={item.url} className='h-full flex items-center px-5 text-white text-lg hover:bg-lime-800'>{item.name}</NavLink></li>
-								})}
-							</ul>
-						</div>
-						<div className='sm:hidden h-full'>
-							<div onClick={() => toggleHamburger()} className='h-full flex items-center cursor-pointer w-8'>
-								<div className={`${state.toggle ? 'flex' : 'flex flex-col'} gap-1`}>
-									<div className={`${state.toggle && 'rotate-45 absolute right-3'} w-8 h-1 bg-white transition-all`}></div>
-									<div className={`${state.toggle ? '' : 'w-8 h-1'} bg-white transition-all `}></div>
-									<div className={`${state.toggle && '-rotate-45 absolute right-3'} w-8 h-1 bg-white transition-all `}></div>
-								</div>
+					<div className='flex'>
+						<nav className='flex'>
+							<div className='hidden sm:block w-full h-full'>
+								<ul className='flex h-full'>
+									{list.map((item) => {
+										return <li key={item.url}><NavLink to={item.url} className='font-semibold h-full flex items-center px-5 text-white text-lg hover:bg-lime-800'>{item.name}</NavLink></li>
+									})}
+								</ul>
 							</div>
-							{state.toggle && <ul className={`absolute w-screen p-5 bg-lime-700 text-white text-2xl text-center  top-14 left-0 z-50`}>
-								{list.map((item) => {
-									return <li key={item.url} className='p-4'><NavLink to={item.url} onClick={() => toggleHamburger()}>{item.name}</NavLink></li>
-								})}
-							</ul>}
 
-						</div>
-					</nav>
+							<Cart />
+
+							<div className='sm:hidden h-full'>
+								<div onClick={() => toggleHamburger()} className='h-full flex items-center cursor-pointer w-8'>
+									<div className={`${state.toggle ? 'flex' : 'flex flex-col'} gap-1`}>
+										<div className={`${state.toggle && 'rotate-45 absolute right-3'} w-8 h-1 bg-white transition-all`}></div>
+										<div className={`${state.toggle ? '' : 'w-8 h-1'} bg-white transition-all `}></div>
+										<div className={`${state.toggle && '-rotate-45 absolute right-3'} w-8 h-1 bg-white transition-all `}></div>
+									</div>
+								</div>
+								{state.toggle && <ul className={`absolute w-screen p-5 bg-lime-700 text-white text-2xl text-center  top-14 left-0 z-50`}>
+									{list.map((item) => {
+										return <li key={item.url} className='p-4'><NavLink to={item.url} onClick={() => toggleHamburger()}>{item.name}</NavLink></li>
+									})}
+								</ul>}
+							</div>
+						</nav>
+					</div>
 				</div>
 			</div>
 		</header >
