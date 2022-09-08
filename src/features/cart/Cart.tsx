e288a5f5c15d5b23eb6502b7b893184e8580649e
NecroBread <mariovyordanov@gmail.com>
2022-09-08 11:53:25 +0300

    create: cart layout

--- src/features/cart/Cart.tsx
+++ src/features/cart/Cart.tsx
@@ -1,24 +1,54 @@
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { cartActions, selectCart } from '../../app/slices/cartSlice';
+import Row from './row/Row';
 
 const Cart = () => {
 	const cart = useAppSelector(selectCart);
 	const dispatch = useAppDispatch();
 
 	return (
-		<div className='h-full fill-white'>
-			<button
-				onClick={() => dispatch(cartActions.toggle())}
-				className='h-full w-full flex items-center px-6 text-white text-lg hover:bg-lime-800'
-			>
-				{cart.items.length === 0
-					? <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M19.029 13h2.971l-.266 1h-2.992l.287-1zm.863-3h2.812l.296-1h-2.821l-.287 1zm-.576 2h4.387l.297-1h-4.396l-.288 1zm2.684-9l-.743 2h-1.929l-3.474 12h-11.239l-4.615-11h14.812l-.564 2h-11.24l2.938 7h8.428l3.432-12h4.194zm-14.5 15c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm5.9-7-.9 7c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z" /></svg>
-					: <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill-rule="evenodd" clip-rule="evenodd"><path d="M13.5 18c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm-3.5 1.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm13.257-14.5h-1.929l-3.473 12h-13.239l-4.616-11h2.169l3.776 9h10.428l3.432-12h4.195l-.743 2zm-13.537 4.183l-2.325-2.183-1.395 1.435 3.746 3.565 6.559-6.592-1.422-1.408-5.163 5.183z" /></svg>
-				}
-			</button>
-			{cart.toggle && <div className='float-right top-16 ml-auto'>
-				<h2>Your cart</h2>
-			</div>}
+		<div className='bg-white p-5 float-right min-w-[30%] w-full sm:w-fit'>
+			<div className='flex gap-9 border-b-2 '>
+				<h2 className='font-bold text-3xl text-primary pb-2'>Shopping Cart</h2>
+				<div className='flex items-center text-xl'>
+					<p>3 items</p>
+				</div>
+			</div>
+
+			<div className=''>
+				<table className='w-full my-3'>
+					<tr className='text-slate-500 uppercase'>
+						<th className='p-1 text-left font-normal text-sm'>Product</th>
+						<th className='p-1 text-left font-normal text-sm'>Quantity</th>
+						<th className='p-1 text-left font-normal text-sm'>Price</th>
+					</tr>
+
+					<Row product={{
+						title: "Kofte",
+						qty: 2,
+						price: 3.50,
+					}} />
+					<Row product={{
+						title: "Musaka",
+						qty: 1,
+						price: 4.50,
+					}} />
+
+					<tr className='font-bold'>
+						<td className='p-1'>Total</td>
+						<td className='p-1'></td>
+						<td className='p-1'>$8.50</td>
+					</tr>
+				</table>
+			</div>
+
+			<div>
+				<button
+					className='disabled:bg-slate-600 hover:bg-lime-800 hover:shadow active:scale-95 transition-all flex w-full justify-center bg-lime-700 font-semibold uppercase py-2 text-white shadow'
+				>
+					Checkout
+				</button>
+			</div>
 		</div>
 	)
 }
